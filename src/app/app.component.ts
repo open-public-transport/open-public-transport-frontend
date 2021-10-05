52ea85e41c0b9470b95872dad99b6ace080f97c4
Florian Schwanz <florian.schwanz@interoberlin.de>
2021-10-05 17:42:15 +0200

    feat: add contact page #6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import {Component} from '@angular/core';
+import {MatIconRegistry} from "@angular/material/icon";
+import {MaterialIconService} from "./core/ui/services/material-icon.service";
+import {DomSanitizer} from "@angular/platform-browser";
 
 /**
  * Displays app component
@@ -12,4 +15,37 @@ export class AppComponent {
 
   /** Title */
   title = 'Open Public Transport';
+
+  /**
+   * Constructor
+   * @param iconRegistry Material icon registry
+   * @param materialIconService Material icon service
+   * @param sanitizer DOM sanitizer
+   */
+  constructor(private iconRegistry: MatIconRegistry,
+              private materialIconService: MaterialIconService,
+              private sanitizer: DomSanitizer) {
+  }
+
+  //
+  // Lifecycle hooks
+  //
+
+  /**
+   * Handles on-init phase
+   */
+  ngOnInit() {
+    this.initializeMaterial();
+  }
+
+  //
+  // Initialization
+  //
+
+  /**
+   * Initializes material colors and icons
+   */
+  private initializeMaterial() {
+    this.materialIconService.initializeIcons(this.iconRegistry, this.sanitizer);
+  }
 }
