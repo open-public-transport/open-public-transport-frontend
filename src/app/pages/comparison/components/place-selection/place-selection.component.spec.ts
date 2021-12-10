b4f3d7af20cfddba10b5a477c3b37f9c4b0fab20
Florian Schwanz <florian.schwanz@interoberlin.de>
2021-12-10 08:05:35 +0100

    feat: add card for geocoder result #20

--- src/app/pages/comparison/components/place-selection/place-selection.component.spec.ts
+++ src/app/pages/comparison/components/place-selection/place-selection.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { PlaceSelectionComponent } from './place-selection.component';
+import {PlaceSelectionComponent} from './place-selection.component';
 
 describe('PlaceSelectionComponent', () => {
   let component: PlaceSelectionComponent;
@@ -8,9 +8,9 @@ describe('PlaceSelectionComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PlaceSelectionComponent ]
+      declarations: [PlaceSelectionComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
