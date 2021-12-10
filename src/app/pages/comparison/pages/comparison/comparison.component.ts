bd3b89b17a17eeb5e9e80898a959864f0b955944
Florian Schwanz <florian.schwanz@interoberlin.de>
2021-12-10 08:09:20 +0100

    feat: add passing geocoder result to parent component #20

--- src/app/pages/comparison/pages/comparison/comparison.component.ts
+++ src/app/pages/comparison/pages/comparison/comparison.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {GeocoderResult} from "../../../../ui/map/model/geocoder-result";
 
+/**
+ * Displays comparison page
+ */
 @Component({
   selector: 'app-comparison',
   templateUrl: './comparison.component.html',
   styleUrls: ['./comparison.component.scss']
 })
-export class ComparisonComponent implements OnInit {
+export class ComparisonComponent {
 
-  constructor() { }
+  /** Selected geocoder result left */
+  resultLeft: GeocoderResult;
+  /** Selected geocoder result right */
+  resultRight: GeocoderResult;
 
-  ngOnInit(): void {
+  //
+  // Actions
+  //
+
+  /**
+   * Handles left geocoder results
+   * @param result geocoder result
+   */
+  onGeocodingResultLeftChanged(result: GeocoderResult) {
+    this.resultLeft = result;
+
+    // TODO Call our endpoint to get metrics
   }
 
+  /**
+   * Handles right geocoder results
+   * @param result geocoder result
+   */
+  onGeocodingResultRightChanged(result: GeocoderResult) {
+    this.resultRight = result;
+
+    // TODO Call our endpoint to get metrics
+  }
 }
