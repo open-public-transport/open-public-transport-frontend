3fc9e1e9e2d0d28ecfdbe4a5a77c2413db0ec26d
Florian Schwanz <florian.schwanz@interoberlin.de>
2021-12-28 12:34:41 +0100

    feat: add place details component #22

---
+++ src/app/pages/comparison/components/place-details/place-details.component.ts
@@ -0,0 +1,123 @@
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {PlaceMetrics} from "../../model/place-metrics";
+import {GeocoderResult} from "../../../../ui/map/model/geocoder-result";
+import {environment} from "../../../../../environments/environment";
+import {StationInformation} from "../../model/station-information";
+import {LineInformation} from "../../model/line-information";
+
+/**
+ * Displays place details
+ */
+@Component({
+  selector: 'app-place-details',
+  templateUrl: './place-details.component.html',
+  styleUrls: ['./place-details.component.scss']
+})
+export class PlaceDetailsComponent implements OnInit, OnChanges {
+
+  /** Place name */
+  @Input() placeName = "";
+  /** Geocoder result */
+  @Input() geocoderResult: GeocoderResult;
+  /** Place metrics of geocoder result */
+  @Input() placeMetrics: PlaceMetrics;
+
+  /** List of available public transport */
+  publicTransport = [];
+
+  //
+  // Lifecycle hooks
+  //
+
+  /**
+   * Handles on-init phase
+   */
+  ngOnInit() {
+  }
+
+  /**
+   * Handles on-changes phase
+   */
+  ngOnChanges(changes: SimpleChanges) {
+    this.initializePublicTransport();
+  }
+
+  //
+  // Initialization
+  //
+
+  /**
+   * Initializes list of public transport
+   */
+  private initializePublicTransport() {
+    if (this.geocoderResult != null) {
+      const cityName = this.geocoderResult.context[2].text;
+      const city = this.getCityByName(cityName);
+
+      this.publicTransport = city.publicTransport;
+    }
+  }
+
+  //
+  // Helpers
+  //
+
+  /**
+   * Returns a city by a given name
+   * @param name name
+   */
+  private getCityByName(name: string) {
+    const cities = environment.dashboard.cities.filter(city => {
+      return city.name === name;
+    });
+
+    return cities[0];
+  }
+
+  /**
+   * Retrieves logo of means of transport
+   * @param transport transport
+   */
+  getTransportLogo(transport: any) {
+    return `../../../../../assets/images/logo_${transport}.png`;
+  }
+
+  /**
+   * Returns station information based in means of transport
+   * @param transport means of transport
+   */
+  getStationInformation(transport: string): StationInformation {
+    if (this.placeMetrics != null && this.placeMetrics.station_information != null) {
+      return this.placeMetrics.station_information.filter(information => {
+        return information.transport_type == transport;
+      })[0];
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * Returns station information based in means of transport
+   * @param transport means of transport
+   */
+  getLineInformation(transport: string): LineInformation {
+    if (this.placeMetrics != null && this.placeMetrics.line_information != null) {
+      return this.placeMetrics.line_information.filter(information => {
+        return information.transport_type == transport;
+      })[0];
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * Returns bike information
+   */
+  getBikeInformation() {
+    if (this.placeMetrics != null && this.placeMetrics.bike_information != null) {
+      return this.placeMetrics.bike_information;
+    } else {
+      return null;
+    }
+  }
+}
