ea1aeaee392ff8d52c224edb449625854b3cc892
Florian Schwanz <florian.schwanz@interoberlin.de>
2021-12-28 12:34:13 +0100

    feat: add bike information to place metrics #22

---
+++ src/app/pages/comparison/model/bike-information.ts
@@ -0,0 +1,7 @@
+/**
+ * Represents bike information
+ */
+export class BikeInformation {
+  /** Bike infrastructure percentage */
+  bike_infrastructure_percentage = 0;
+}
--- src/app/pages/comparison/model/place-metrics.ts
+++ src/app/pages/comparison/model/place-metrics.ts
@@ -1,6 +1,7 @@
 import {StationInformation} from "./station-information";
 import {LineInformation} from "./line-information";
 import {TravelDistanceInformation} from "./travel-distance-information";
+import {BikeInformation} from "./bike-information";
 
 /**
  * Represents place metrics
@@ -12,6 +13,8 @@ export class PlaceMetrics {
   station_information: StationInformation[];
   /** Line information */
   line_information: LineInformation[];
+  /** Bike information */
+  bike_information: BikeInformation;
   /** Travel distance information */
   travel_distance_information: TravelDistanceInformation[];
 }
